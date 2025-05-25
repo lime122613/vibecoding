6269c98b0103f68ec1aee0ce8bd2719b1c391c95
lime122613 <167674932+lime122613@users.noreply.github.com>
2025-05-25 15:58:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ def calc_fee(row, total_minutes, day_type):
     return fee
     
 
-df = pd.read_csv("https://raw.githubusercontent.com/lime122613/vibecoding/refs/heads/main/seoul_public_parking.csv", encoding="cp949")
+df = pd.read_csv("https://raw.githubusercontent.com/lime122613/vibecoding/main/seoul_public_parking.csv", encoding="cp949")
 df = df.dropna(subset=['위도', '경도'])
 df['구'] = df['주소'].apply(lambda x: x.split()[0] if '구' in x else '')
 
