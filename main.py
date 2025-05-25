ce6bceb40336a48504528e6b934b08ceb68f37e8
lime122613 <167674932+lime122613@users.noreply.github.com>
2025-05-25 14:19:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -47,8 +47,7 @@ if uploaded_file:
     for _, row in filtered.iterrows():
         tooltip_text = (
             f"총 주차면: {int(row['총 주차면'])}개<br>"
-            f"기본 주차 요금: {int(row['기본 주차 요금'])}원 - " 
-            f"기본 주차 시간: {row['기본 주차 시간(분 단위)']}분당<br>"
+            f"기본 주차 요금: {int(row['기본 주차 요금'])}원 ({int(row['기본 주차 시간(분 단위)'])}분당)<br> " 
             f"평일 운영: {format_time(row['평일 운영 시작시각(HHMM)'])} ~ {format_time(row['평일 운영 종료시각(HHMM)'])}<br>"
             f"주말 운영: {format_time(row['주말 운영 시작시각(HHMM)'])} ~ {format_time(row['주말 운영 종료시각(HHMM)'])}<br>"
             f"공휴일 운영: {format_time(row['공휴일 운영 시작시각(HHMM)'])} ~ {format_time(row['공휴일 운영 종료시각(HHMM)'])}<br>"
